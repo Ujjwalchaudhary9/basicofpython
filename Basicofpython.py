74020771fec7772f6f8052be7bbf9e4c60c62d70
Ujjwal Chaudhary <121443255+Ujjwalchaudhary9@users.noreply.github.com>
2023-09-14 21:34:16 +0530

    Add files via upload

---
+++ Basicofpython.py
@@ -0,0 +1,158 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ### print hello world
+
+# In[1]:
+
+
+print("Hello World")
+
+
+# In[2]:
+
+
+print('Hello World')
+
+
+# In[3]:
+
+
+print(Hello World)
+
+
+# In[4]:
+
+
+print(10.7)
+
+
+# In[5]:
+
+
+First name="Ujjwal"
+
+
+# In[7]:
+
+
+Firstname="Ujjwal"
+Firstname
+
+
+# In[9]:
+
+
+_name="gg"
+_name
+
+
+# In[10]:
+
+
++name="jhhf"
++name
+
+
+# In[11]:
+
+
+1name="kjhh"
+1name
+
+
+# In[12]:
+
+
+n1me="jh"
+n1me
+
+
+# In[13]:
+
+
+first+name="jhuf"
+first+name
+
+
+# In[14]:
+
+
+first_name="jbdhb"
+first_name
+
+
+# In[15]:
+
+
+x=10
+type(x)
+
+
+# In[16]:
+
+
+x=10.5
+type(x)
+
+
+# In[17]:
+
+
+z=True
+type(z)
+
+
+# In[18]:
+
+
+z="true"
+type(z)
+
+
+# In[19]:
+
+
+z=true
+type(z)
+
+
+# In[20]:
+
+
+x=10
+type(x)
+x
+
+
+# In[21]:
+
+
+x,type(x)
+
+
+# In[22]:
+
+
+x,y=10,20
+y
+x
+
+
+# In[23]:
+
+
+x,y
+
+
+# In[24]:
+
+
+y,x
+
+
+# In[ ]:
+
+
+
+
